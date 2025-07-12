970cfc62d4ec0f7efa5b7e3a3e6402e3c1308074
Chris Waring <106938+cwaring@users.noreply.github.com>
2025-07-12 23:41:42 +0100

    feat: implement mermaid diagrams

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import mermaid from 'astro-mermaid';
 import sidebarConfig from './sidebar-config.json';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
+		mermaid({
+			theme: 'default',
+			autoTheme: true
+		}),
 		starlight({
 			title: 'UCAN Specification',
 			social: [
